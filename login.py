28841324446de115a7bf6ea511a82c58d91ea516
abdoessordo <abdoessordo01@gmail.com>
2022-05-22 18:56:42 +0100

    walo

--- login.py
+++ login.py
@@ -2,8 +2,9 @@ import json
 import requests
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522 as rfid
+from time import sleep 
 
-
+GPIO.setwarnings(False)
 def read_tag():
     reader = rfid()
     try:
@@ -13,21 +14,31 @@ def read_tag():
     finally:
         GPIO.cleanup()
 
+def get_patient_id(card_id):
+    DATA = {"card": card_id}
+    r = requests.post("http://3.18.29.109/card/", DATA)
+    print(r.status_code)
+    if r.status_code == 200 and r.status_code != {'error': 'Not found'}:
+        return r.json()['pat']
 
 def add_visit():
     patient_id = -999999
     while True:
-        with open("./inp.json", "r") as inp_f:
+        with open("/home/pi/Desktop/e-health-appareil/inp.json", "r") as inp_f:
             inp = json.load(inp_f)['INP']
         inp_f.close()
-        patient_id = print(read_tag())
+        patient_id = read_tag()
         if(patient_id != -999999):
+            patient_id = get_patient_id(patient_id)
+            print(patient_id)
+            print('scanned')       
             DATA = {"patient": patient_id,
-                    "medcin": medcin_id
+                    "medcin": inp
                     }
             r = requests.post("http://3.18.29.109/doctor/create_visite", DATA)
 
             print(r.status_code)
+            sleep(2)
 
 
 if __name__ == '__main__':
--- main/views.py
+++ main/views.py
@@ -5,23 +5,18 @@ import requests
 import json
 
 
-# Create your views here.
-
-def hello(request):
-    return render(request, 'index.html')
-
+# Create your views here
 
 def login(request):
     cin = request.POST['cin']
     password = request.POST['password']
     status_code, response_data = get_inp(cin, password)
-    print(response_data)
-    if status_code == 200:
-        with open("./inp.json", "w") as inp_file:
+    if response_data != {'error': 'Not found'}:
+        with open("/home/pi/Desktop/e-health-appareil/inp.json", "w") as inp_file:
             json.dump(response_data, inp_file)
         inp_file.close
     return HttpResponse(json.dumps(response_data), content_type="application/json")
-
+    return HttpResponse(status_code)
 
 def get_inp(cin, password):
 
@@ -29,5 +24,5 @@ def get_inp(cin, password):
         "username": cin,
         "password": password
     }
-    r = requests.post("http://3.18.29.109/json_check/", DATA)
+    r = requests.get("http://3.18.29.109/json_check/", params=DATA)
     return r.status_code, r.json()
